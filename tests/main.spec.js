c6fe64dcbac0a40b2400ce6f53187941fc344bb7
khaueviana <khaueviana@gmail.com>
2018-03-04 11:44:56 -0300

    Refactor tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -34,6 +34,19 @@ describe('Spotify Wrapper', () => {
       const artists = search();
 
       expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore();
+    });
+
+    it('should receive the correct URL to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Incubus', 'artist');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albums = search('Incubus', 'album');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
     });
   });
 });

ba338a0644260d450a32e7f8f13578532688724e
khaueviana <khaueviana@gmail.com>
2018-03-03 23:24:46 -0300

    Add smoke tests

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbums = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
